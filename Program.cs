801f86f9af9d40a44dd2e540773c02ca89fc172a
islam atef nagy <78918678+Islamatef100@users.noreply.github.com>
2023-09-23 22:57:37 +0300

    Add files via upload
    this code create just one object from class and not allowed to create more

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace OneInstanceOnly
+{
+    // this class to create just one object from class 
+    class CreatOneInstance
+    {
+        private static CreatOneInstance instance;
+        private CreatOneInstance()
+        {
+            Console.WriteLine("you created object");
+        }
+        public static CreatOneInstance creatObject()
+        {
+            if (instance == null)
+            {
+                CreatOneInstance.instance = new CreatOneInstance();
+                return CreatOneInstance.instance;
+            }
+            else
+            {
+                throw new Exception("you can not create more one object from this class");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            CreatOneInstance obj2 = CreatOneInstance.creatObject();
+            try {
+                CreatOneInstance obj3 = CreatOneInstance.creatObject();
+            } catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+           
+            
+           
+
+        }
+    }
+}
